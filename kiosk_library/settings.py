b8786ba527a875a766b0219be4020c0bc7fd8663
amieldanao <amielrenaissance4@gmail.com>
2023-02-12 20:14:24 +0800

    added STATIC_ROOT

--- kiosk_library/settings.py
+++ kiosk_library/settings.py
@@ -147,7 +147,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 STATIC_URL = "static/"
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'staticfiles'),
