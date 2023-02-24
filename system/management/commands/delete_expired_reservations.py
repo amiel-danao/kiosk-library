c8da5ec2f450b3ea23f2d124b0513dbad3d8ed47
amieldanao <amielrenaissance4@gmail.com>
2023-02-25 02:18:54 +0800

    corrected make_aware

--- system/management/commands/delete_expired_reservations.py
+++ system/management/commands/delete_expired_reservations.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db.models.functions import Now
 from django.utils import timezone
+from django.utils.timezone import make_aware
 from datetime import datetime, timedelta
 from system.models import BookInstance, BookStatus, Reservations
 
@@ -8,8 +9,8 @@ class Command(BaseCommand):
     help = 'Remove expired reservations'
 
     def handle(self, *args, **options):
-        d = datetime.now() - timedelta(hours=1)
-        all = Reservations._base_manager.first()
+        d = make_aware(datetime.now() - timedelta(hours=1))
+
         qs = Reservations._base_manager.filter(expiry_date__lt=d)
         
         ids = qs.values_list('book_instance__pk', flat=True)
